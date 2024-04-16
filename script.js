116e9d195eeed9e6385f7f47682ae5606b290896
marta <105271643+ffanecaa@users.noreply.github.com>
2024-04-16 12:30:15 +0200

    titles

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 
     let menu = document.querySelector('.menu');
     let hamb = document.querySelector('.hamb');
-    let container = document.querySelector('.container');
+   
     hamb.addEventListener('click', () => {
     
     menu.classList.toggle('active') // Agrega o quita la clase 'active' al menú
